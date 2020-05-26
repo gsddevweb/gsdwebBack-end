f43e7d86b712c687a8c1c9b4028f6e7d4d3d3fa2
George7205 <ngugigeorge697@gmail.com>
2020-05-26 15:06:00 +0300

    Laravel All

---
+++ app/Http/Controllers/LevelOfEducationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LevelOfEducation;
+use Illuminate\Http\Request;
+
+class LevelOfEducationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LevelOfEducation  $levelOfEducation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LevelOfEducation $levelOfEducation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LevelOfEducation  $levelOfEducation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LevelOfEducation $levelOfEducation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LevelOfEducation  $levelOfEducation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LevelOfEducation $levelOfEducation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LevelOfEducation  $levelOfEducation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LevelOfEducation $levelOfEducation)
+    {
+        //
+    }
+}
