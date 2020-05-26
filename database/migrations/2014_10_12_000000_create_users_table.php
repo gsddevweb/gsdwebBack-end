83d86827de95f41221530b3b8325ae9e31a0177e
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-05-26 15:16:25 +0300

    Revert "Laravel All"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,27 +1,9 @@
 <?php
-/**
- * MyClass Class Doc Comment
- * php version 7.2.10
- *
- * @category Class
- * @package  MyPackage
- * @author   George Kariuki <ngugigeorge697@gmail.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://www.hashbangcode.com/
- */
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-/**
- * MyClass Class Doc Comment
- * php version 7.2.10
- *
- * @category Class
- * @package  MyPackage
- * @author   George Kariuki <ngugigeorge697@gmail.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://www.hashbangcode.com/
- */
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -31,20 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-
-
-        Schema::create(
-            'users',
-            function (Blueprint $table) {
-                $table->engine = "InnoDB";
-                $table->bigIncrements('id');
-                $table->string('email')->unique();
-                $table->timestamp('email_verified_at')->nullable();
-                $table->string('password');
-                $table->rememberToken();
-                $table->timestamps();
-            }
-        );
+		
+		
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
