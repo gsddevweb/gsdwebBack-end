362e55f1ffed6248f005776f113a2007e88051d7
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-06-08 21:53:07 +0300

    Added API routes

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/levelOfEducation', 'LevelOfEducationController');
+Route::Resource('levelOfEducation', 'LevelOfEducationController');
+Route::Resource('subjects','SubjectController');
+// Route::Resource('users','User');
+Route::Resource('chapters','ChapterController');
